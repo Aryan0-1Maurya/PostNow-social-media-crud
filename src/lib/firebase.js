6490ef9fdefa66fc7d9851299d45ae5c3bf31a6b
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2024-01-27 01:02:24 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,18 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   apiKey: "AIzaSyCca26qIexM72QTLzGGGJcMoHJ0o644_h0",
+  authDomain: "create-post-social.firebaseapp.com",
+  projectId: "create-post-social",
+  storageBucket: "create-post-social.appspot.com",
+  messagingSenderId: "526859699328",
+  appId: "1:526859699328:web:acd5acce776c40e0465265"
 };
 
 export const app = initializeApp(firebaseConfig);
